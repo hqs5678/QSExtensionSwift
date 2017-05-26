02088f0669781c9df2697fe912d857012aa6a7e3
黄青松 <huangqingsong_5678@hotmail.com>
2017-05-26 10:12:34 +0800

    update string

--- classes/String+Size.swift
+++ classes/String+Size.swift
@@ -20,8 +20,14 @@ extension String {
         return newStr.boundingRect(with: size, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSFontAttributeName:font], context: nil)
     }
     
+    // 去除字符串前后字符
+    public func trim(_ characters: String) -> String{
+        return self.trimmingCharacters(in: CharacterSet(charactersIn: characters))
+    }
+    
+    // 去除字符串前后空格
     public func trim() -> String {
-        return self.trimmingCharacters(in: CharacterSet(charactersIn: " "))
+        return self.trimmingCharacters(in: .whitespaces)
     }
     
     public func toDataValue() -> Data? {
--- test-searchview-swift/ViewController.swift
+++ test-searchview-swift/ViewController.swift
@@ -12,40 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let str = "01234567890"
+       
+        var str = "     123   "
+        str = str.trim()
+        print(str)
         print(str.length)
-        print(str.substringToIndex(3))
-        print(str.substringFromIndex(3))
-        print(str.substringFromIndex(3, toIndex: 5))
         
-        print(str.substringToIndex(-1))
-        print(str.substringFromIndex(33))
-        print(str.substringFromIndex(-2, toIndex: 2))
-        
-        print(str.indexOf(string: "34"))
-        
-        let s = "1..2.3.4.5"
-        print(s.toArrayBy(separator: ".."))
-        
-     print(str.charAt(index: 2))
-        print(str[0])
-        
-        let num = 1234567
-        print(num[9])
-        
-        
-        let array = Array(arrayLiteral: 1, 2, 3)
-        print(array[2])
-        
-        let a = NSArray(arrayLiteral: 1, 2, 3)
-        print(a[0])
-        
-        
-        self.view.addTapWithHandle { (tap) in
-            
-        }
-        
+        str = "http://www.baidu.com"
+        print(str.trim("http://"))
     }
 
     
